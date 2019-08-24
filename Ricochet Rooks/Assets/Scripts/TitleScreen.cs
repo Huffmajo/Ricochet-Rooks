e62752160704e40e04d8b2ad0edea927901a7267
Huffmajo <huffmajo@oregonstate.edu>
2019-08-24 16:57:35 -0700

    Title screen, settings and level select scenes created.

---
+++ Ricochet Rooks/Assets/Scripts/TitleScreen.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreen : MonoBehaviour
+{
+	public string levelsSceneName;
+	public string settingsSceneName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // 
+    public void levels()
+    {
+    	SceneManager.LoadScene(levelsSceneName);
+    }
+
+	public void settings()
+    {
+    	SceneManager.LoadScene(settingsSceneName);
+    }
+
+    // close out application
+    public void quitGame()
+    {
+    	Application.Quit();
+    }
+}
--- Ricochet Rooks/Assets/Scripts/TitleScreenCube.cs
+++ Ricochet Rooks/Assets/Scripts/TitleScreenCube.cs
@@ -6,18 +6,21 @@ public class TitleScreenCube : MonoBehaviour
 {
 	public float maxRotate;
 	public float minRotate;
+	public float rotationSpeed;
 	public float rotateX;
 	public float rotateY;
 	public float rotateZ;
-	public bool self;
+	public int rotationStyle;
 
     // Start is called before the first frame update
     void Start()
     {
-    	self = true;
+    	rotationStyle = 0;
+    	rotationSpeed = 1.0f;
         maxRotate = 2.0f;
 		minRotate = -2.0f;
 	
+		// paint cube red
 		GetComponent<Renderer>().material.color = Color.red;
 
 		randomizeSpeeds();
@@ -26,19 +29,64 @@ public class TitleScreenCube : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-    	if (Input.GetAxis("Jump") > 0.2f)
+    	if (Input.GetKeyDown(KeyCode.Space))
     	{
-    		randomizeSpeeds();
+    		if (rotationStyle >= 5)
+    		{
+    			rotationStyle = 0;
+    		}
+    		else
+    		{
+    			rotationStyle += 1;
+    		}
     	}
 
-    	if (self)
+    	switch (rotationStyle)
     	{
-    		transform.Rotate(rotateX, rotateY, rotateZ, Space.Self);	
+    		case 0:
+    			rotateX = rotationSpeed;
+    			rotateY = 0f;
+    			rotateZ = 0f;
+    			break;
+
+    		case 1:
+    			rotateX = 0f;
+    			rotateY = rotationSpeed;
+    			rotateZ = 0f;
+    			break;
+
+    		case 2:
+    			rotateX = 0f;
+    			rotateY = 0f;
+    			rotateZ = rotationSpeed;
+    			break;
+
+    		case 3:
+    			rotateX = rotationSpeed * -1;
+    			rotateY = 0f;
+    			rotateZ = 0f;
+    			break;
+
+    		case 4:
+    			rotateX = 0f;
+    			rotateY = rotationSpeed * -1;
+    			rotateZ = 0f;
+    			break;
+
+			case 5:
+    			rotateX = 0f;
+    			rotateY = 0f;
+    			rotateZ = rotationSpeed * -1;
+    			break;
+
+    		default:
+    			rotateX = 0f;
+    			rotateY = 0f;
+    			rotateZ = 0f;
+    			break;
     	}
-        else
-        {
-        	transform.Rotate(rotateX, rotateY, rotateZ, Space.World);	
-        }
+
+   		transform.Rotate(rotateX, rotateY, rotateZ, Space.Self);	
     }
 
     void randomizeSpeeds()
