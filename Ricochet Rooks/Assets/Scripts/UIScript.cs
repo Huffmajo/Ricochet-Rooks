28e1a6be7acb0bdf11b371a87590795102415e32
Huffmajo <huffmajo@oregonstate.edu>
2019-08-28 14:56:14 -0700

    Condensed UI scripts into a single file. Added percentage painted UI element.

---
+++ Ricochet Rooks/Assets/Scripts/UIScript.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIScript : MonoBehaviour
+{
+	public Text movesText;
+	public Text tilesLeft;
+	public GameObject player;
+	public GameObject[] tiles;
+
+	private int numMoves;
+	private int totalTiles;
+	private int paintedTiles;
+	public float percentage;
+	
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    	// get all tiles into array
+    	tiles = GameObject.FindGameObjectsWithTag("FloorTile");
+
+    	// calculate total
+    	totalTiles = tiles.Length;
+
+    	// print UI elements
+    	showNumMoves();
+    	showNumTilesLeft();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	// print UI elements
+    	showNumMoves();
+		showNumTilesLeft();
+    }
+
+    void showNumMoves()
+    {
+    	// get number of moves from player object
+    	numMoves = player.GetComponent<RookScript>().numMoves;
+
+    	// print results to UI
+    	movesText.text = "Moves: " + numMoves;
+    }
+
+    void showNumTilesLeft()
+    {
+    	// initialize variables
+    	paintedTiles = 0;
+
+    	// check all tiles to see if they are painted
+    	foreach (GameObject tile in tiles)
+    	{
+    		if (tile.GetComponent<Renderer>().material.color != Color.white)
+    		{
+    			paintedTiles += 1;
+    		}
+    	}
+
+    	percentage = ((paintedTiles * 1.0f) / (totalTiles * 1.0f)) * 100f;
+
+
+
+    	// print results to UI
+    	tilesLeft.text = "Tiles Left: " + paintedTiles + "/" + totalTiles + "\n" + Mathf.RoundToInt(percentage) + "%";
+    }
+}
